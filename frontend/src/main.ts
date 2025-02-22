26379c0881ed47ca9623d4a27608e1a1945df199
Bladesheng <keadr23@gmail.com>
2025-02-22 10:24:39 +0100

    change primary color to yellow

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,14 +9,33 @@ import Aura from '@primevue/themes/aura';
 import ToastService from 'primevue/toastservice';
 import ConfirmationService from 'primevue/confirmationservice';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import { definePreset } from '@primevue/themes';
 
 const app = createApp(App);
+
+const MyPreset = definePreset(Aura, {
+	semantic: {
+		primary: {
+			50: '{yellow.50}',
+			100: '{yellow.100}',
+			200: '{yellow.200}',
+			300: '{yellow.300}',
+			400: '{yellow.400}',
+			500: '{yellow.500}',
+			600: '{yellow.600}',
+			700: '{yellow.700}',
+			800: '{yellow.800}',
+			900: '{yellow.900}',
+			950: '{yellow.950}',
+		},
+	},
+});
 
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, {
 	theme: {
-		preset: Aura,
+		preset: MyPreset,
 		options: {
 			darkModeSelector: '.dark',
 
