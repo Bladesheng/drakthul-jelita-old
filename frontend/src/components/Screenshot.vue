e54979a9bdc855eb4448866b11f3c3804d2716cc
Bladesheng <keadr23@gmail.com>
2025-03-01 11:17:57 +0100

    change screenshot hover cursor for logged-in user

--- frontend/src/components/Screenshot.vue
+++ frontend/src/components/Screenshot.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import type { IScreenshot } from '@/types/types.ts';
+import { useUser } from '@/stores/user.ts';
 
 const { screenshot } = defineProps<{
 	screenshot: IScreenshot;
 }>();
 
+const userStore = useUser();
+
 const VITE_S3_URL = import.meta.env.VITE_S3_URL;
 </script>
 
 <template>
-	<img :src="`${VITE_S3_URL}/${screenshot.path}`" :alt="screenshot.wow_name" />
+	<img
+		:src="`${VITE_S3_URL}/${screenshot.path}`"
+		:alt="screenshot.wow_name"
+		:class="{ 'cursor-pointer': userStore.isLoggedIn }"
+	/>
 </template>
 
 <style scoped></style>
